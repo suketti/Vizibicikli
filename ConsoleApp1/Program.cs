d70ed946dfe70ff552a5d687c4534aaddb1e2eeb
suketti <54027485+suketti@users.noreply.github.com>
2023-05-22 14:20:55 +0200

    kesz

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,51 @@
 {
     internal class Program
     {
-        List<Rentals> list = new List<Rentals>();
+        static List<Rentals> list = new List<Rentals>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StreamReader sr = new StreamReader("kolcsonzesek.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream) 
+            {
+                var fields = sr.ReadLine().Split(';');
+                Rentals rental = new Rentals(fields[0], fields[1][0], int.Parse(fields[2]), int.Parse(fields[3]), int.Parse(fields[4]), int.Parse(fields[5]));
+
+                list.Add(rental);
+            }
+            if (list.Count(x=>x.Name == "Kata") == 0)
+            {
+                Console.WriteLine("Nem volt ilyen kolcsonzes");
+            } else
+            {
+                list.Where(x => x.Name == "Kata")
+                    .ToList()
+                    .ForEach(x => Console.WriteLine($"{x.SHour}:{x.SMinute}-{x.EHour}:{x.EMinute}"));
+            }
+
+            Console.Write("7. feladat: Adjon meg egy időpontot óra:perc ablakban: ");
+            string inputTime = Console.ReadLine();
+            int inputHour = Convert.ToInt32(inputTime.Split(':')[0]);
+            int inputMinute = Convert.ToInt32(inputTime.Split(':')[1]);
+            int osszido = inputHour * 60 + inputMinute;
+
+            list.Where(x => x.SHour * 60 + x.SMinute < osszido && osszido < x.EHour * 60 + x.EMinute).ToList()
+                .ForEach(x => Console.WriteLine($"{x.SHour}:{x.SMinute}-{x.EHour}:{x.EMinute} : {x.Name}"));
+
+
+            int dailyIncome = 2400 * (list.Sum(x => x.timeLength()) / 30 + 1);
+            Console.WriteLine($"8. feladat: A napi bevétel: {dailyIncome} Ft");
+
+            StreamWriter sw = new StreamWriter("F.txt");
+            list.Where(x => x.VIden == 'F')
+                .ToList()
+                .ForEach(x => sw.WriteLine($"{x.SHour} : {x.SMinute} - {x.SHour}:{x.SMinute} : {x.Name}"));
+            sw.Close();
+
+            list.GroupBy(x => x.VIden)
+               .OrderBy(x => x.Key)
+               .ToList()
+               .ForEach(x => Console.WriteLine($"{x.Key} - {x.Count()}"));
         }
     }
 }
--- ConsoleApp1/Rentals.cs
+++ ConsoleApp1/Rentals.cs
@@ -6,15 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Rentals
+    public  class Rentals
     {
-        string name;
+        private string name;
         private char vIden;
         private int sHour;
         private int sMinute;
         private int eHour;
         private int eMinute;
 
+        public int timeLength()
+        {
+            return (eHour * 60) + eMinute - (sHour * 60) + sMinute;
+        }
 
+        public Rentals(string Name, char VIden, int SHour, int SMinute, int EHour, int EMinute)
+        {
+            this.name = Name;
+            this.vIden = VIden;
+            this.sHour = SHour;
+            this.sMinute = SMinute;
+            this.eHour = EHour;
+            this.eMinute = EMinute;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public char VIden { get => vIden; set => vIden = value; }
+        public int SHour { get => sHour; set => sHour = value; }
+        public int SMinute { get => sMinute; set => sMinute = value; }
+        public int EHour { get => eHour; set => eHour = value; }
+        public int EMinute { get => eMinute; set => eMinute = value; }
     }
 }
